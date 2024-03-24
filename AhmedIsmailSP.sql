87206acc12c837057320e581694252d1306da255
Logan-Kranis <142769039+Logan-Kranis@users.noreply.github.com>
2024-03-24 10:59:03 -0400

    Update AhmedIsmailSP.sql

--- AhmedIsmailSP.sql
+++ AhmedIsmailSP.sql
@@ -1,35 +1,26 @@
-CREATE or Alter PROCEDURE [dbo].[InsertFarm]
-    @address nvarchar(max),
-    @zipcode nvarchar(max),
-    @city nvarchar(max),
-    @state nvarchar(max),
-    @name nvarchar(max),
-    @FARM_ID int
+CREATE or ALTER PROCEDURE [dbo].[UpdateFarm]
+    @FARM_ID INT,
+    @address NVARCHAR(MAX),
+    @zipcode NVARCHAR(MAX),
+    @city NVARCHAR(MAX),
+    @state NVARCHAR(MAX),
+    @name NVARCHAR(MAX)
 AS
 BEGIN
-    INSERT INTO [dbo].[farm] ([address], [zipcode], [city], [state], [name], [FARM_ID])
-    VALUES (@address, @zipcode, @city, @state, @name, @FARM_ID);
+    UPDATE [dbo].[farm]
+    SET [address] = @address,
+        [zipcode] = @zipcode,
+        [city] = @city,
+        [state] = @state,
+        [name] = @name
+    WHERE [FARM_ID] = @FARM_ID;
 END;
 GO
-
-/*
-EXEC [dbo].[InsertFarm]
-    @address = N'123 Farm Lane',
-    @zipcode = N'12345',
-    @city = N'Farmville',
-    @state = N'Farming State',
-    @name = N'Happy Farm',
-    @FARM_ID = 1;
-*/
-
-CREATE or Alter PROCEDURE [dbo].[SelectFarmsByCity]
-    @city nvarchar(max)
+CREATE or ALTER PROCEDURE [dbo].[DeleteFarm]
+    @FARM_ID INT
 AS
 BEGIN
-    SELECT * FROM [dbo].[farm]
-    WHERE [city] = @city;
+    DELETE FROM [dbo].[farm]
+    WHERE [FARM_ID] = @FARM_ID;
 END;
 GO
-/*
-EXEC [dbo].[SelectFarmsByCity] @city = N'Farmville';
-*/
